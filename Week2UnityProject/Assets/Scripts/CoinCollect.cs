76e753d023449461154948dd1cef64b2a654083e
Atinder Singh <atinder.singh@surreyschools.ca>
2022-12-05 09:50:42 -0800

    Dec 05

--- Week2UnityProject/Assets/Scripts/CoinCollect.cs
+++ Week2UnityProject/Assets/Scripts/CoinCollect.cs
@@ -2,10 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-public class LoadLevel3 : MonoBehaviour
-{
 
-    public CoinCollect cC;
+public class CoinCollect : MonoBehaviour
+{
+    public int points = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,9 +15,15 @@ public class LoadLevel3 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (cC.points == 4)
+        if (points == 4)
         {
+            
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
+    }
+
+    private void OnGUI()
+    {
+        GUI.Label(new Rect(10, 10, 100, 20), "Score : " + points);
     }
 }
